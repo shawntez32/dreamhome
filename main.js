f9e815441f60b20cef26cedfe2bd0b9dc8e38e45
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2022-05-11 03:21:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const search =document.getElementById('myInput');
+const matchList = document.getElementById('matchList');
+
+const searchContent = async searchText => {
+    const res = await fetch('states.json');
+    const state = await res.json();
+
+    let matches = state.filter(state => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return state.name.match(regex) || state.abbr.match(regex);
+    });
+
+    if (searchText.length === 0) {
+        matches = [];
+    };
+
+    let txt = "";
+    matches.forEach(myFunction);
+    console.log(matches.length);
+    document.getElementById("matchList").innerHTML = txt;
+
+    function myFunction(value) {
+      txt += "<h4><a href=>" + value.name + "<span> " + value.abbr + "</span></a></h4><br>";
+}};
+
+search.addEventListener('input', () => searchContent(search.value));
