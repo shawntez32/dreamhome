fbcfb4031e4636dd70488a282c41a5664fc36f96
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2022-07-17 23:11:48 -0400

    Add files via upload

---
+++ components/SuperHeader.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Super() {
+    return (
+    <>
+    <div>
+    <div className="nav2">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Our mission</li>
+    </div>
+        <h2>Tez that guy its undeniable</h2>
+    </div>
+    </>
+    );
+}
+
+export default Super;
