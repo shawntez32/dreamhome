84a7a2097beb8232ee4c15917073028f3e244004
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2022-08-01 10:45:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ const searchContent = async searchText => {
     matches.forEach(myFunction);
     console.log(matches.length);
     document.getElementById("matchList").innerHTML = txt;
+    document.getElementById("matchList").style = 'background-color:grey';
 
     function myFunction(value) {
       txt += `<h4><a href=>${value.name},<span> ${value.abbr}</span></a></h4><br>`;
